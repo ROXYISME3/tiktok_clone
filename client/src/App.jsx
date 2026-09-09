d20e30b290e6af7c9d76f7566ef1c7879d4a4398
ROXYISME3 <clenista.roxan@cec.edu.ph>
2026-09-09 15:57:55 +0800

    Fix signup and login API

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -92,11 +92,9 @@ function App() {
 
       const response = await fetch(`${API_URL}/api/signup`, {
         method: "POST",
-
         headers: {
           "Content-Type": "application/json",
         },
-
         body: JSON.stringify({
           username: username,
           phone: phone,
