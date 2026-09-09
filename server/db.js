f302e02226cacde3241b0f2e268a8b5055afb306
ROXYISME3 <clenista.roxan@cec.edu.ph>
2026-09-09 09:36:33 +0800

    Fix Railway CORS and database configuration

--- server/db.js
+++ server/db.js
@@ -1,6 +1,7 @@
-const mysql = require("mysql2/promise");
-require("dotenv").config();
-
-const db = mysql.createPool(process.env.MYSQL_URL);
-
-module.exports = db;
+const db = mysql.createPool({
+  host: process.env.MYSQLHOST,
+  port: Number(process.env.MYSQLPORT),
+  user: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
+  database: process.env.MYSQLDATABASE,
+});
